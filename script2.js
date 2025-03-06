3ce45123a1d99939491edb5ef5f7b126d602988f
magalhaesf <160252530+magalhaesf@users.noreply.github.com>
2025-03-06 18:43:51 -0300

    Atividade 24 Verificar se duas strings são iguais.

---
+++ script2.js
@@ -0,0 +1,13 @@
+var coisa = String( prompt("Digite alguma coisa") );
+var outra = String( prompt("Digite outra coisa") );
+
+if (coisa == outra) {
+
+alert("São termos iguais!");
+
+} else {
+
+alert("São termos diferentes!");
+
+
+}
